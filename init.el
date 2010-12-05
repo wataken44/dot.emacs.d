9de6556680d5d113b926d69c92d126f80cb84e51
wataken44 <wataken44@iris.local>
2010-12-05 18:20:35 +0900

    moved miscellanous setting into init-misc.el

--- init.el
+++ init.el
@@ -11,16 +11,6 @@
       (cons (expand-file-name "~/.emacs.d/preference/")
 	    load-path))
 
-; font-lock-mode
-(global-font-lock-mode t)
-
-; short cut
-(global-set-key "\C-cg" 'goto-line)
-(global-set-key "\C-cr" 'replace-string)
-(global-set-key "\C-cR" 'query-replace)
-(global-set-key "\C-c\M-r" 'replace-regexp)
-(global-set-key "\C-ci" 'indent-region)
-
 ; add-hook for each mode
 (add-hook 'python-mode-hook
 	  '(lambda ()
@@ -46,16 +36,10 @@
 	     (setq sgml-basic-offset 2)
 	     (setq indent-tabs-mode nil)))
 
-; do not show startup message
-(setq inhibit-startup-message t)
+; miscellanous init
+(load "init-misc")
 
-; ansi color on shell
-(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
-
-; do not show tool-bar
-(tool-bar-mode 0)
-
-; load init file for each environment
+; system-dependent init
 (load "init-system-dependent")
 
 ; autoinsert init
---
+++ preference/init-misc.el
@@ -0,0 +1,27 @@
+;; init-misc.el
+;; miscellanous setting
+
+; font-lock-mode
+(global-font-lock-mode t)
+
+; short cut
+(global-set-key "\C-cg" 'goto-line)
+(global-set-key "\C-cr" 'replace-string)
+(global-set-key "\C-cR" 'query-replace)
+(global-set-key "\C-c\M-r" 'replace-regexp)
+(global-set-key "\C-ci" 'indent-region)
+
+; do not show startup message
+(setq inhibit-startup-message t)
+
+; ansi color on shell
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+
+; do not show tool-bar
+(tool-bar-mode 0)
+
+; do not show menu-bar
+(menu-bar-mode 0)
+
+; do not use tab by default
+(setq-default indent-tabs-mode nil)
