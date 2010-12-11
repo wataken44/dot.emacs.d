4da982cd5273c3171287557521b4c1305698b6d4
wataken44 <wataken44@iris.local>
2010-12-11 22:47:16 +0900

    implement next-user-buffer

--- preference/init-misc.el
+++ preference/init-misc.el
@@ -4,14 +4,41 @@
 ; font-lock-mode
 (global-font-lock-mode t)
 
+; define function
+(defun next-user-buffer ()
+  ;; switch to next user's buffer
+  ;; name of user's buffer start with [^ *]
+  (interactive)
+  ;; get current buffer index in buffer-list
+  (setq buffer-list-length (length (buffer-list))) 
+  (setq current-buffer-index 
+        (- buffer-list-length
+            (length (member (current-buffer) (buffer-list)))
+            )
+        )
+  ;; search and jump to next user buffer
+  ;; start from current-buffer-index + 1 and wrap around
+  (dotimes (i buffer-list-length)
+    (setq buffer-index
+          (% (+ i current-buffer-index 1) buffer-list-length)
+          )
+    (setq dest-buffer (nth buffer-index (buffer-list)))
+    (setq dest-buffer-name (buffer-name dest-buffer))
+    (if (not (string-match-p "^[ *]" dest-buffer-name))
+        (progn (switch-to-buffer dest-buffer)
+               (return t))
+      )
+    )
+  )
+
 ; short cut
 (global-set-key "\C-cg" 'goto-line)
 (global-set-key "\C-cr" 'replace-string)
 (global-set-key "\C-cR" 'query-replace)
 (global-set-key "\C-c\M-r" 'replace-regexp)
 (global-set-key "\C-ci" 'indent-region)
-(global-set-key "\C-t" 'next-buffer)
-(global-set-key "\C-T" 'previous-buffer) 
+(global-set-key "\C-t" 'next-user-buffer)
+(global-set-key "\C-T" 'next-buffer) 
 
 ; do not show startup message
 (setq inhibit-startup-message t)
@@ -24,6 +51,9 @@
 
 ; do not show menu-bar
 (menu-bar-mode 0)
+
+; show counter-side paren
+(show-paren-mode 1)
 
 ; region mark visible
 (setq-default transient-mark-mode t)
