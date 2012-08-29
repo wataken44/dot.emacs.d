e37ca30a754d0383b97487683bf3a3191f32a14f
Kenichi Watanabe <wataken44@maria.local>
2012-08-29 22:42:44 +0900

    fix: unmatch width between japanese font and ascii font

--- preference/init-cocoa.el
+++ preference/init-cocoa.el
@@ -1,12 +1,13 @@
 ;;; font setting
 ;;; copied from http://sakito.jp/emacs/emacs23.html#id15
-(create-fontset-from-ascii-font "Menlo-15:weight=normal:slant=normal" nil "menlokakugo")
-(set-fontset-font "fontset-menlokakugo"
+(create-fontset-from-ascii-font "Menlo-15:weight=normal:slant=normal" nil "menlomarugo")
+(set-fontset-font "fontset-menlomarugo"
                   'unicode
                   (font-spec :family "Hiragino Maru Gothic ProN" :size 15)
                   nil
                   'append)
-(add-to-list 'default-frame-alist '(font . "fontset-menlokakugo"))
+(setq face-font-rescale-alist '(("^-apple-hiragino.*" . 1.2)))
+(add-to-list 'default-frame-alist '(font . "fontset-menlomarugo"))
 
 ; show menubar (at top of display, not in frame)
 (menu-bar-mode 1)
